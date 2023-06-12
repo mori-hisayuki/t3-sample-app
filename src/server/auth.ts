c254267b9a913893ac885aa6c20937b0f8bf405a
hisayuki_mori <h.mori0206@gmail.com>
2023-06-13 03:32:27 +0900

    signInページの作成

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,13 +1,9 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { type GetServerSidePropsContext } from "next";
-import {
-  getServerSession,
-  type NextAuthOptions,
-  type DefaultSession,
-} from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import { env } from "~/env.mjs";
-import { prisma } from "~/server/db";
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { type GetServerSidePropsContext } from 'next'
+import { getServerSession, type NextAuthOptions, type DefaultSession } from 'next-auth'
+import GitHubProvider from 'next-auth/providers/github'
+import { env } from '~/env.mjs'
+import { prisma } from '~/server/db'
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -15,15 +11,15 @@ import { prisma } from "~/server/db";
  *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session extends DefaultSession {
     user: {
-      id: string;
+      id: string
       /*
        * ...other properties
        * role: UserRole;
        */
-    } & DefaultSession["user"];
+    } & DefaultSession['user']
   }
 
   /*
@@ -45,9 +41,9 @@ export const authOptions: NextAuthOptions = {
       ...session,
       user: {
         ...session.user,
-        id: user.id,
-      },
-    }),
+        id: user.id
+      }
+    })
   },
   adapter: PrismaAdapter(prisma),
   providers: [
@@ -65,7 +61,10 @@ export const authOptions: NextAuthOptions = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
-};
+  pages: {
+    signIn: '/signin'
+  }
+}
 
 /**
  * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
@@ -73,8 +72,8 @@ export const authOptions: NextAuthOptions = {
  * @see https://next-auth.js.org/configuration/nextjs
  */
 export const getServerAuthSession = (ctx: {
-  req: GetServerSidePropsContext["req"];
-  res: GetServerSidePropsContext["res"];
+  req: GetServerSidePropsContext['req']
+  res: GetServerSidePropsContext['res']
 }) => {
-  return getServerSession(ctx.req, ctx.res, authOptions);
-};
+  return getServerSession(ctx.req, ctx.res, authOptions)
+}
