23014862be9d31e5016005e5c0a41db6b6e43295
hisayuki_mori <h.mori0206@gmail.com>
2023-07-05 02:50:20 +0900

    console.logの削除

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,18 +1,9 @@
-import { createNextApiHandler } from "@trpc/server/adapters/next";
-import { env } from "~/env.mjs";
-import { appRouter } from "~/server/api/root";
-import { createTRPCContext } from "~/server/api/trpc";
+import { createNextApiHandler } from '@trpc/server/adapters/next'
+import { appRouter } from '~/server/api/root'
+import { createTRPCContext } from '~/server/api/trpc'
 
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
-  createContext: createTRPCContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-          );
-        }
-      : undefined,
-});
+  createContext: createTRPCContext
+})
